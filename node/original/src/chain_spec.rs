a51cf949017ee3e668cb654bf9015b00a3836483
NamNg <namduy1012@gmail.com>
2023-03-13 15:52:27 +0900

    PoW-V2 finished generate PoW

--- node/original/src/chain_spec.rs
+++ node/original/src/chain_spec.rs
@@ -1,12 +1,13 @@
 use original_runtime::{
-	AccountId, AuraConfig, BalancesConfig, GenesisConfig, GrandpaConfig, Signature,
+	AccountId, BalancesConfig, GenesisConfig, Signature,
 	SudoConfig, SystemConfig, WASM_BINARY,
-	DifficultyConfig, RewardsConfig, TreasuryConfig
+	DifficultyConfig, RewardsConfig, TreasuryConfig,
+	// GrandpaConfig, AuraConfig // Change Here
 };
 use sc_service::ChainType;
-use sp_consensus_aura::sr25519::AuthorityId as AuraId;
+// use sp_consensus_aura::sr25519::AuthorityId as AuraId; // Change Here
+// use sp_finality_grandpa::AuthorityId as GrandpaId;// Change Here
 use sp_core::{sr25519, Pair, Public};
-use sp_finality_grandpa::AuthorityId as GrandpaId;
 use sp_runtime::traits::{IdentifyAccount, Verify};
 
 // The URL for the telemetry server.
@@ -33,9 +34,9 @@ where
 }
 
 /// Generate an Aura authority key.
-pub fn authority_keys_from_seed(s: &str) -> (AuraId, GrandpaId) {
-	(get_from_seed::<AuraId>(s), get_from_seed::<GrandpaId>(s))
-}
+// pub fn authority_keys_from_seed(s: &str) -> (AuraId, GrandpaId) {
+// 	(get_from_seed::<AuraId>(s), get_from_seed::<GrandpaId>(s))
+// } // Change Here
 
 pub fn development_config() -> Result<ChainSpec, String> {
 	let wasm_binary = WASM_BINARY.ok_or_else(|| "Development wasm not available".to_string())?;
@@ -50,7 +51,7 @@ pub fn development_config() -> Result<ChainSpec, String> {
 			testnet_genesis(
 				wasm_binary,
 				// Initial PoA authorities
-				vec![authority_keys_from_seed("Alice")],
+				// vec![authority_keys_from_seed("Alice")], // Change Here
 				// Sudo account
 				get_account_id_from_seed::<sr25519::Public>("Alice"),
 				// Pre-funded accounts
@@ -90,12 +91,12 @@ pub fn local_testnet_config() -> Result<ChainSpec, String> {
 			testnet_genesis(
 				wasm_binary,
 				// Initial PoA authorities
-				vec![
-					authority_keys_from_seed("Alice"),
-					authority_keys_from_seed("Bob"),
-					authority_keys_from_seed("Eve"),
-					authority_keys_from_seed("Charlie"),
-				],
+				// vec![
+				// 	authority_keys_from_seed("Alice"),
+				// 	authority_keys_from_seed("Bob"),
+				// 	authority_keys_from_seed("Eve"),
+				// 	authority_keys_from_seed("Charlie"),
+				// ],// Change Here
 				// Sudo account
 				get_account_id_from_seed::<sr25519::Public>("Alice"),
 				// Pre-funded accounts
@@ -133,7 +134,7 @@ pub fn local_testnet_config() -> Result<ChainSpec, String> {
 /// Configure initial storage state for FRAME modules.
 fn testnet_genesis(
 	wasm_binary: &[u8],
-	initial_authorities: Vec<(AuraId, GrandpaId)>,
+	// initial_authorities: Vec<(AuraId, GrandpaId)>,// Change Here
 	root_key: AccountId,
 	endowed_accounts: Vec<AccountId>,
 	_enable_println: bool,
@@ -147,12 +148,12 @@ fn testnet_genesis(
 			// Configure endowed accounts with initial balance of 1 << 60.
 			balances: endowed_accounts.iter().cloned().map(|k| (k, 1 << 60)).collect(),
 		},
-		aura: AuraConfig {
-			authorities: initial_authorities.iter().map(|x| (x.0.clone())).collect(),
-		},
-		grandpa: GrandpaConfig {
-			authorities: initial_authorities.iter().map(|x| (x.1.clone(), 1)).collect(),
-		},
+		// aura: AuraConfig {
+		// 	authorities: initial_authorities.iter().map(|x| (x.0.clone())).collect(),
+		// }, // Change Here
+		// grandpa: GrandpaConfig {
+		// 	authorities: initial_authorities.iter().map(|x| (x.1.clone(), 1)).collect(),
+		// }, // Change Here
 		sudo: SudoConfig {
 			// Assign network admin rights.
 			key: Some(root_key),
